43f2f27e8eb7d54cdb4ac73f8e499652f97b5c30
Doug Kintop <dkintop@hotmail.com>
2019-09-18 23:20:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -85,7 +85,13 @@ class TicTacToe
       if @board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" 
         && @board[win_combo[2]] == "X"
         win_combo 
-      elsif 
+      elsif @board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" 
+        && @board[win_combo[2]] == "O"
+        win_combo 
+      else 
+        false
+      end 
+    end 
     
     
   end 
