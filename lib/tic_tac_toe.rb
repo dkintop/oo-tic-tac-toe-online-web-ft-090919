9a51741216a5bfe6bea51441dd0d3e896b2caf46
Doug Kintop <dkintop@hotmail.com>
2019-09-18 23:44:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -117,14 +117,7 @@ end
     end 
   end 
   
-  def winner
-    if (@board[winning_combo[0]]) == "X" && (@board[winning_combo[1]]) == "X" && (@board[winning_combo[2]]) == "X"
-        return "X"
-    elsif(@board[winning_combo[0]]) == "O" && (@board[winning_combo[1]])== "O" && (@board[winning_combo[2]]) == "O" 
-        return "O"
-    else 
-    false
-  end
+  
     
   end
 
